b262e97def0d643fad6eedc3df4752926e58b323
istaion <v.poutot@gmail.com>
2025-01-24 16:33:36 +0100

    hop

--- Djang_Assurance/Djang_Assurance/urls.py
+++ Djang_Assurance/Djang_Assurance/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    path('utilisateurs/', include('user.urls'))
+    path('utilisateurs/', include('user.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
