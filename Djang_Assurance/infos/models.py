03672faedd629479b40859804ba68023aa29d55c
ludivine <ludivine.raby@gmail.com>
2025-01-28 13:37:15 +0100

    addition of a complete footer

--- Djang_Assurance/infos/models.py
+++ Djang_Assurance/infos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ContactBase(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    mail = models.CharField(max_length=250, null=False)
+    subject = models.CharField(max_length=100)
+    message = models.TextField(null=True)
--- Djang_Assurance/infos/urls.py
+++ Djang_Assurance/infos/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import NewsView
+from .views import (
+    NewsView,
+    AboutView,
+    PrivacyView,
+    ContactView,
+)
+
 
 urlpatterns = [
     path('news/', NewsView.as_view(), name='news'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('privacy/', PrivacyView.as_view(), name='privacy'),
+    path('contact/', ContactView.as_view(), name='contact'),
 
 ]
