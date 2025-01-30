ea9614a40e7caa3c983fbce929c904dc8cdb1bf1
istaion <v.poutot@gmail.com>
2025-01-30 16:31:05 +0100

    dernere tentative

--- Djang_Assurance/Djang_Assurance/settings.py
+++ Djang_Assurance/Djang_Assurance/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-DEBUG= os.getenv('DEBUG', 'True') == 'True'
+DEBUG= False
 
 ALLOWED_HOSTS = ['django-assurance.onrender.com', 'localhost', '127.0.0.1']
 
